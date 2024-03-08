7a5cdb8a2dd07fb43eb39a5ab580d573d8daf914
ilansav25 <ilan84988@gmail.com>
2024-03-08 15:32:13 +0200

    third commit page-27 addded

---
+++ page27.js
@@ -0,0 +1,9 @@
+console.log("=====ex1=======")
+var topNumber = Number(prompt("Enter number top"))
+var index = 1;
+
+while(index <= topNumber){
+    console.log("The Next number is: " +index)
+    index = index + 1;
+}
+console.log("finished")
